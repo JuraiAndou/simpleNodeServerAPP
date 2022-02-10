e6dcbdd96c544e355254c5173f11297b8d426e1f
Samuel Afonso <samuel.gomes.afonso@gmail.com>
2022-02-10 19:12:31 -0300

    app final

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 const http = require('http')
-const fs = require('fs')
+const url = require("url")
+const stringDecoder = require("string_decoder").StringDecoder
+const util = require("util")
+//const formidable = require("formidable")
+
+const fs = require('fs').promises
+const host = 'localhost'
 const port = 3000
 
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type' : 'text/html'})
-    fs.readFile('index.html', function(error, data){
-        if(error){
-            res.writeHead(404)
-            res.write('Error: File not found')
-        }else{
-            res.write(data)
-        }
-        res.end()
-    })
-})
+let indexFile
 
-server.listen(port, function (error) {
-    if (error)
-        console.log('Somenthing went wrong buddy', error)
-    else
-        console.log('Server is listening to port ' + port)
-})
+const requestListener = function (req, res) {
+    let path = url.parse(req.url, true)
+    //path.pathname path.search path.query --path, querystring, qs object
+    //path.port path.protocol path.origin -- all nulls
+
+    res.setHeader("Content-Type", "text/html")
+    res.writeHead(200)
+    if(req.method.toLowerCase() == "get"){
+        qs = path.query
+        if(qs.valor){
+            res.write(indexFile)
+
+            val = parseInt(qs.valor) + 2
+            res.write("<h2 style=\'color:red\'>Valor da soma: " + val.toString() + "</h2>")
+            res.end()
+        }else{
+            res.end(indexFile)
+        }
+    }
+    else{
+        res.end(indexFile)
+    }
+}
+
+const server = http.createServer(requestListener)
+
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents
+        server.listen(port, host, () =>{
+            console.log(`Server is running on http://${host}:${port}`)
+        })
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`)
+        process.exit
+    })
